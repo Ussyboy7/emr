1d678337e8e3c17636d01417b4fe62d1147f0c0c
MacBook <macbook@MacBooks-MacBook-Pro.local>
2026-04-20 15:56:54 +0100

    Fix docker-compose file paths in production scripts to use deployment/ directory

--- scripts/production/emr-prod-manager.sh
+++ scripts/production/emr-prod-manager.sh
@@ -6,7 +6,7 @@
 set -e
 
 # Configuration
-COMPOSE_FILE="docker-compose.prod.yml"
+COMPOSE_FILE="deployment/docker-compose.prod.yml"
 BACKUP_DIR="$HOME/emr_backups"
 LOG_DIR="./logs"
 MONITOR_LOG="./monitoring.log"
